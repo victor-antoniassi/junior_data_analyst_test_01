21ed2c2689c4526b73457ee4d7356cac43cee38f
Victor Antoniassi <github.finite060@slmails.com>
2025-01-08 19:59:52 -0300

    docs(etl.py): translate comments and docstrings from Portuguese to English

--- etl/etl.py
+++ etl/etl.py
@@ -8,52 +8,52 @@ from prefect.logging import get_run_logger
 @task(retries=3, retry_delay_seconds=5)
 def download_csv(filename: str, save_directory: str, downloaded_csv_path: Path, logger: get_run_logger) -> None:
     """
-    Baixa um arquivo CSV de uma URL específica e o salva localmente.
+    Downloads a CSV file from a specific URL and saves it locally.
 
     Args:
-        filename (str): Nome do arquivo CSV a ser baixado.
-        save_directory (str): Diretório onde o arquivo será salvo.
-        downloaded_csv_path (Path): Caminho completo para o arquivo CSV baixado.
-        logger (get_run_logger): Logger para registrar informações sobre o processo.
+        filename (str): Name of the CSV file to be downloaded.
+        save_directory (str): Directory where the file will be saved.
+        downloaded_csv_path (Path): Complete path for the downloaded CSV file.
+        logger (get_run_logger): Logger to record process information.
 
     Raises:
-        URLError: Se ocorrer um erro durante o download do arquivo.
+        URLError: If an error occurs during file download.
     """
-    logger.info(f"Iniciando download de {filename}...")
+    logger.info(f"Starting download of {filename}...")
     download_url = 'https://drive.google.com/uc?export=download&id=1eoy8MlYin9PxbCjozT0kjPXPsq0RXEgY'
     logger.info(f"Downloading {filename} from {download_url} to {save_directory}")
     wget.download(download_url, out=str(downloaded_csv_path))
-    logger.info(f"Download completo.")
+    logger.info(f"Download complete.")
 
 @task
 def to_pandas(filename: str, downloaded_csv_path: Path, logger: get_run_logger) -> pd.DataFrame:
     """
-    Lê um arquivo CSV e o converte em um DataFrame pandas.
+    Reads a CSV file and converts it to a pandas DataFrame.
 
     Args:
-        filename (str): Nome do arquivo CSV a ser lido.
-        downloaded_csv_path (Path): Caminho para o arquivo CSV.
-        logger (get_run_logger): Logger para registrar informações sobre o processo.
+        filename (str): Name of the CSV file to be read.
+        downloaded_csv_path (Path): Path to the CSV file.
+        logger (get_run_logger): Logger to record process information.
 
     Returns:
-        pd.DataFrame: DataFrame contendo os dados do arquivo CSV.
+        pd.DataFrame: DataFrame containing the CSV file data.
     """
-    logger.info(f"Lendo {filename} em um DataFrame pandas...")
+    logger.info(f"Reading {filename} into a pandas DataFrame...")
     df = pd.read_csv(downloaded_csv_path, sep=';', encoding='latin-1').reset_index().rename(columns={'index': 'index'})
-    logger.info(f"Arquivo {filename} lido com sucesso. Shape: {df.shape}")
+    logger.info(f"File {filename} successfully read. Shape: {df.shape}")
     return df
 
 @task
 def create_table(df: pd.DataFrame, conn: duckdb.DuckDBPyConnection, logger: get_run_logger) -> None:
     """
-    Cria a tabela 'games_sales' no DuckDB a partir de um DataFrame.
+    Creates the 'games_sales' table in DuckDB from a DataFrame.
 
     Args:
-        df (pd.DataFrame): DataFrame contendo os dados a serem inseridos na tabela.
-        conn (duckdb.DuckDBPyConnection): Conexão com o banco de dados DuckDB.
-        logger (get_run_logger): Logger para registrar informações sobre o processo.
+        df (pd.DataFrame): DataFrame containing the data to be inserted into the table.
+        conn (duckdb.DuckDBPyConnection): Connection to the DuckDB database.
+        logger (get_run_logger): Logger to record process information.
     """
-    logger.info("Criando tabela 'games_sales' no DuckDB...")
+    logger.info("Creating 'games_sales' table in DuckDB...")
     conn.sql("DROP TABLE IF EXISTS games_sales;")
     query = """
     CREATE TABLE games_sales AS
@@ -63,49 +63,49 @@ def create_table(df: pd.DataFrame, conn: duckdb.DuckDBPyConnection, logger: get_
     FROM df;
     """
     conn.sql(query)
-    logger.info("Tabela 'games_sales' criada com sucesso.")
+    logger.info("Table 'games_sales' created successfully.")
 
 @task
 def format_sales_values(conn: duckdb.DuckDBPyConnection, logger: get_run_logger) -> None:
     """
-    Formata os valores de vendas na tabela 'games_sales' para o tipo DOUBLE e divide por 1000.
+    Formats the sales values in the 'games_sales' table to DOUBLE type and divides by 1000.
 
     Args:
-        conn (duckdb.DuckDBPyConnection): Conexão com o banco de dados DuckDB.
-        logger (get_run_logger): Logger para registrar informações sobre o processo.
+        conn (duckdb.DuckDBPyConnection): Connection to the DuckDB database.
+        logger (get_run_logger): Logger to record process information.
     """
-    logger.info("Formatando valores de vendas na tabela 'games_sales'...")
+    logger.info("Formatting sales values in 'games_sales' table...")
     columns = ["Sales_NA", "Sales_EU", "Sales_JP", "Sales_Others", "Global_Sales"]
     alter_sql = "ALTER TABLE games_sales ALTER COLUMN $column_name SET DATA TYPE DOUBLE USING $column_name / 1000.0;"
     for column in columns:
         conn.sql(alter_sql.replace("$column_name", column))
-    logger.info("Valores de vendas formatados com sucesso.")
+    logger.info("Sales values formatted successfully.")
 
 @task
 def load_to_csv(conn: duckdb.DuckDBPyConnection, logger: get_run_logger) -> None:
     """
-    Exporta os dados da tabela 'games_sales' para um arquivo CSV.
+    Exports the data from 'games_sales' table to a CSV file.
 
     Args:
-        conn (duckdb.DuckDBPyConnection): Conexão com o banco de dados DuckDB.
-        logger (get_run_logger): Logger para registrar informações sobre o processo.
+        conn (duckdb.DuckDBPyConnection): Connection to the DuckDB database.
+        logger (get_run_logger): Logger to record process information.
     """
-    logger.info("Exportando dados da tabela 'games_sales' para um arquivo CSV...")
+    logger.info("Exporting data from 'games_sales' table to CSV file...")
     conn.sql("""
     COPY games_sales TO 'data/games_sales.csv' (HEADER, DELIMITER ';')
     """)
-    logger.info("Dados exportados com sucesso.")
+    logger.info("Data exported successfully.")
 
 @flow
 def etl_games_sales() -> None:
     """
-    Executa o fluxo de ETL (Extract, Transform, Load) para processar dados de vendas de jogos.
+    Executes the ETL (Extract, Transform, Load) flow to process game sales data.
 
-    1. Baixa o arquivo CSV contendo os dados de vendas.
-    2. Lê o CSV em um DataFrame pandas.
-    3. Cria uma tabela 'games_sales' no DuckDB a partir do DataFrame.
-    4. Formata os valores de vendas na tabela para o tipo DOUBLE e divide por 1000.
-    5. Exporta os dados da tabela 'games_sales' para um arquivo CSV.
+    1. Downloads the CSV file containing sales data.
+    2. Reads the CSV into a pandas DataFrame.
+    3. Creates a 'games_sales' table in DuckDB from the DataFrame.
+    4. Formats the sales values in the table to DOUBLE type and divides by 1000.
+    5. Exports the data from 'games_sales' table to a CSV file.
 
     Args:
         None
@@ -119,7 +119,7 @@ def etl_games_sales() -> None:
     downloaded_csv_path = Path(f'{save_directory}/{filename}')
     duckdb_path = Path('data/db_games_sales.db')
     conn: duckdb.DuckDBPyConnection = duckdb.connect(duckdb_path)
-    logger.info("Iniciando pipeline ETL...")
+    logger.info("Starting ETL pipeline...")
     
     # EXTRACT
     download_csv(filename, save_directory, downloaded_csv_path, logger)
@@ -132,7 +132,7 @@ def etl_games_sales() -> None:
     # LOAD
     load_to_csv(conn, logger)
     
-    logger.info("Pipeline ETL concluído com sucesso.")
+    logger.info("ETL pipeline completed successfully.")
     conn.close()
 
 if __name__ == "__main__":
